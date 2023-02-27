821d579905c3692a890f2e23d47d30fcaf5db584
mesri <abde_mes@yahoo.fr>
2023-02-27 16:35:58 +0100

    Mulithread oldStartFinder

--- OldStarsFinder/OldStarsFinder/Form1.Designer.cs
+++ OldStarsFinder/OldStarsFinder/Form1.Designer.cs
@@ -28,18 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            picStarsBitmap = new PictureBox();
+            butFindOldStars = new Button();
+            ((System.ComponentModel.ISupportInitialize)picStarsBitmap).BeginInit();
             SuspendLayout();
+            // 
+            // picStarsBitmap
+            // 
+            picStarsBitmap.Image = (Image)resources.GetObject("picStarsBitmap.Image");
+            picStarsBitmap.Location = new Point(102, 175);
+            picStarsBitmap.Name = "picStarsBitmap";
+            picStarsBitmap.Size = new Size(1017, 761);
+            picStarsBitmap.TabIndex = 0;
+            picStarsBitmap.TabStop = false;
+            // 
+            // butFindOldStars
+            // 
+            butFindOldStars.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            butFindOldStars.Location = new Point(130, 73);
+            butFindOldStars.Name = "butFindOldStars";
+            butFindOldStars.Size = new Size(246, 72);
+            butFindOldStars.TabIndex = 1;
+            butFindOldStars.Text = "Find old start";
+            butFindOldStars.UseVisualStyleBackColor = true;
+            butFindOldStars.Click += butFindOldStars_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1474, 1129);
+            Controls.Add(butFindOldStars);
+            Controls.Add(picStarsBitmap);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)picStarsBitmap).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox picStarsBitmap;
+        private Button butFindOldStars;
     }
 }
--- OldStarsFinder/OldStarsFinder/Form1.cs
+++ OldStarsFinder/OldStarsFinder/Form1.cs
@@ -1,10 +1,190 @@
+using System.Drawing.Imaging;
+
 namespace OldStarsFinder
 {
     public partial class Form1 : Form
     {
+        // The number of processors or cores available in the computer for this application
+        private int priProcessorCount = Environment.ProcessorCount;
+
+        // The bitmaps list
+        private List<Bitmap> prloBitmapList;
+
+        // The long list with the old stars count
+        private List<long> prliOldStarsCount;
+
+        // The threads list
+        private List<Thread> prloThreadList;
+
+        // The original huge infrared bitmap portrait
+        Bitmap proOriginalBitmap;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private Bitmap CropBitmap(Bitmap proBitmap, Rectangle proRectangle)
+        {
+            // Create a new bitmap copying the portion of the original 
+            // defined by proRectangle and keeping its PixelFormat
+            Bitmap loCroppedBitmap = proBitmap.Clone(proRectangle, proBitmap.PixelFormat);
+
+            // Return the cropped bitmap
+            return loCroppedBitmap;
+        }
+
+        public bool IsOldStar(Color poPixelColor)
+        {
+            // Hue between 150 and 258
+            // Saturation more than 0.10
+            // Brightness more than 0.90
+            return ((poPixelColor.GetHue() >= 150) &&
+             (poPixelColor.GetHue() <= 258) &&
+             (poPixelColor.GetSaturation() >= 0.10) &&
+             (poPixelColor.GetBrightness() <= 0.90));
+        }
+
+
+        private void ThreadOldStarsFinder(object poThreadParameter)
+        {
+            // Retrieve the thread number received in object poThreadParameter
+            int liThreadNumber = (int)poThreadParameter;
+
+            // The pixel matrix (bitmap) row number (Y)
+            int liRow;
+
+            // The pixel matrix (bitmap) col number (X)
+            int liCol;
+
+            // The pixel color
+            Color loPixelColor;
+            // Get my bitmap part from the bitmap list
+            Bitmap loBitmap = prloBitmapList[liThreadNumber];
+            // Reset my old stars counter
+            prliOldStarsCount[liThreadNumber] = 0;
+            // Iterate through each pixel matrix (bitmap) row
+            for (liRow = 0; liRow < loBitmap.Height; liRow++)
+            {
+                // Iterate through each pixel matrix (bitmap) cols
+                for (liCol = 0; liCol < loBitmap.Width; liCol++)
+                {
+                    // Get the pixel color for liCol and liRow
+                    loPixelColor = loBitmap.GetPixel(liCol, liRow);
+                    if (IsOldStar(loPixelColor))
+                    {
+                        // The color range corresponds to an old star
+                        // Change its color to a pure blue
+                        loBitmap.SetPixel(liCol, liRow, Color.Blue);
+                        // Increase the old stars counter
+                        prliOldStarsCount[liThreadNumber]++;
+                    }
+                }
+            }
+        }
+
+
+        private void WaitForThreadsToDie()
+        {
+            // A bool flag
+            bool lbContinue = true;
+            int liDeadThreads = 0;
+            int liThreadNumber;
+            while (lbContinue)
+            {
+                for (liThreadNumber = 0; liThreadNumber < priProcessorCount; liThreadNumber++)
+                {
+                    if (prloThreadList[liThreadNumber].IsAlive)
+                    {
+                        // One of the threads is still alive, 
+                        // exit the for loop and sleep 100 
+                        // milliseconds
+                        break;
+                    }
+                    else
+                    {
+                        // Increase the dead threads count
+                        liDeadThreads++;
+                    }
+                }
+                if (liDeadThreads == priProcessorCount)
+                {
+                    // All the threads are dead, exit the while 
+                    // loop
+                    break;
+                }
+                Thread.Sleep(100);
+                liDeadThreads = 0;
+            }
+        }
+
+        private void ShowBitmapWithOldStars()
+        {
+          
+            Bitmap loBitmap;
+            // The starting row in each iteration
+            int liStartRow = 0;
+            // Calculate each bitmap's height
+            int liEachBitmapHeight = ((int)(proOriginalBitmap.Height / priProcessorCount)) + 1;
+            // Create a new bitmap with the whole width and 
+            // height
+            loBitmap = new Bitmap(proOriginalBitmap.Width, proOriginalBitmap.Height);
+
+            Graphics g = Graphics.FromImage((Image)loBitmap);
+            g.InterpolationMode = System.Drawing.Drawing2D.
+            InterpolationMode.HighQualityBicubic;
+            for (int liThreadNumber = 0; liThreadNumber < priProcessorCount; liThreadNumber++)
+            {
+                // Draw each portion in its corresponding 
+                // absolute starting row
+                g.DrawImage(prloBitmapList[liThreadNumber], 0, liStartRow);
+                // Increase the starting row
+                liStartRow += liEachBitmapHeight;
+            }
+            // Show the bitmap in the PictureBox picStarsBitmap
+            picStarsBitmap.Image = loBitmap;
+            g.Dispose();
+        }
+
+        private void butFindOldStars_Click(object sender, EventArgs e)
+        {
+            proOriginalBitmap = new Bitmap(picStarsBitmap.Image);
+      
+            // Create the thread list; the long list and the bitmap list
+            prloThreadList = new List<Thread>(priProcessorCount);
+            prliOldStarsCount = new List<long>(priProcessorCount);
+            prloBitmapList = new List<Bitmap>(priProcessorCount);
+            int liStartRow = 0;
+            int liEachBitmapHeight = ((int)(proOriginalBitmap.Height / priProcessorCount)) + 1;
+            int liHeightToAdd = proOriginalBitmap.Height;
+            Bitmap loBitmap;
+            // Initialize the threads
+            for (int liThreadNumber = 0; liThreadNumber < priProcessorCount; liThreadNumber++)
+            {
+                // Just to occupy the number
+                prliOldStarsCount.Add(0);
+                if (liEachBitmapHeight > liHeightToAdd)
+                {
+                    // The last bitmap height perhaps is less than the other bitmaps height
+                    liEachBitmapHeight = liHeightToAdd;
+                }
+                loBitmap = CropBitmap(proOriginalBitmap, new Rectangle(0, liStartRow, proOriginalBitmap.Width, liEachBitmapHeight));
+                liHeightToAdd -= liEachBitmapHeight;
+                liStartRow += liEachBitmapHeight;
+                prloBitmapList.Add(loBitmap);
+                // Add the new thread, with a parameterized start 
+                // (to allow parameters)
+                prloThreadList.Add(new Thread(new ParameterizedThreadStart(ThreadOldStarsFinder)));
+
+            }
+            // Now, start the threads
+            for (int liThreadNumber = 0; liThreadNumber < priProcessorCount; liThreadNumber++)
+            {
+                prloThreadList[liThreadNumber].Start(liThreadNumber);
+            }
+            WaitForThreadsToDie();
+            ShowBitmapWithOldStars();
+        }
     }
+
 }
