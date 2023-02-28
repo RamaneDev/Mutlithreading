bd89a66ed5572f9150669d701910b5eacfdfe0b2
mesri <abde_mes@yahoo.fr>
2023-02-28 16:35:28 +0100

    Lunching Tasks with TaskFactory object provided by .Net, TaskFactory allow us to set more mutch params to customise the task scheduling

---
+++ Muti_thread_using_TPL/TaskFactoryExample/TaskFactoryExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskFactoryExample
+{
+    internal class Program
+    {
+        static TaskFactory TF = new TaskFactory(TaskScheduler.Default);
+
+        static void Main(string[] args)
+        {
+            List<Task> tasklist = new List<Task>();
+            tasklist.Add(TF.StartNew(() => Worker("Task 1"), CancellationToken.None, TaskCreationOptions.PreferFairness, TaskScheduler.Default));
+            tasklist.Add(TF.StartNew(() => Worker("Task 2"), CancellationToken.None, TaskCreationOptions.PreferFairness, TaskScheduler.Default));
+            tasklist.Add(TF.StartNew(() => Worker("Task 3"), CancellationToken.None, TaskCreationOptions.PreferFairness, TaskScheduler.Default));
+            tasklist.Add(TF.StartNew(() => Worker("Task 4"), CancellationToken.None, TaskCreationOptions.PreferFairness, TaskScheduler.Default));
+            tasklist.Add(TF.StartNew(() => Worker("Task 5"), CancellationToken.None, TaskCreationOptions.PreferFairness, TaskScheduler.Default));
+            //wait for all tasks to complete.
+            Task.WaitAll(tasklist.ToArray());
+            //Wait for input before ending program.
+
+            Console.ReadLine();
+        }
+        static void Worker(String taskName)
+        {
+            Console.WriteLine("This is Task - {0}", taskName);
+        }
+    }
+    
+}
