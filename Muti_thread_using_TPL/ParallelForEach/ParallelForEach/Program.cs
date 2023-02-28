7d780ca585980631f8bd0b424f1189a9acf675da
mesri <abde_mes@yahoo.fr>
2023-02-28 18:01:41 +0100

    using ParallelForeach to loop in IEnumerable by multiple thread

---
+++ Muti_thread_using_TPL/ParallelForEach/ParallelForEach/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] localStrings = "I am doing a simple example of a Parallel foreach loop".Split(' ');
+             Parallel.ForEach(localStrings, currentString =>
+                                                          {
+             Console.WriteLine("Current word is - {0}, and the current thread is - {1} ", currentString, Thread.CurrentThread.ManagedThreadId);
+              }); 
+             Console.ReadLine();
+        }
+    }
+}
