4be38792e800120882702b20b57487a72f479519
mesri <abde_mes@yahoo.fr>
2023-03-01 13:38:53 +0100

    waiting for a task that return a value.

--- Muti_thread_using_TPL/WaitonTask/WaitonTask/Program.cs
+++ Muti_thread_using_TPL/WaitonTask/WaitonTask/Program.cs
@@ -6,6 +6,7 @@ namespace WaitonTask
 {
     internal class Program    {
 
+        static Random ran = new Random();
         static void Main(string[] args)
         {
             // Wait on a single task with no timeout.
@@ -15,7 +16,12 @@ namespace WaitonTask
             
             // Wait on a single task with a timeout.
             Task taskB = Task.Factory.StartNew(() => Worker(2000000));
-            
+
+            Task<double> taskC = Task<double>.Factory.StartNew(() => Worker1());
+            Console.WriteLine("TaskC finished = result is {0}.", taskC.Result); // here the task return a value, so we do not need to wait()
+                                                                                // for this task the .Net will wait automatically by calling task.Result
+                                                                                // but for the tasks that does not return value, wa must wait for them explicitly
+
             taskB.Wait(2000); //Wait for 2 seconds and Go on -> taskB will always be running
             if (taskB.IsCompleted)
                 Console.WriteLine("Task B Finished.");
@@ -28,5 +34,13 @@ namespace WaitonTask
         {
             Thread.Sleep(waitTime);
         }
+
+        static double Worker1()
+        {
+            int i = ran.Next(1000000);
+            Thread.SpinWait(i);
+            return i;
+        }
+
     }
 }
