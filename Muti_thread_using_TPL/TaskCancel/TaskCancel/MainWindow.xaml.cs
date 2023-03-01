d8074e6570ca83a51441a9a786511b00ee687298
mesri <abde_mes@yahoo.fr>
2023-03-01 19:05:08 +0100

    cancel task exemple using TPL Token class

---
+++ Muti_thread_using_TPL/TaskCancel/TaskCancel/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskCancel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource ts;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public double AddMultiple(int number)
+        {
+            double result = 1;
+            for (int i = 1; i < 100000000; i++)
+            {
+                ts.Token.ThrowIfCancellationRequested(); // this exception will be handled by the .NET framework
+                result = result + (number * i);
+            }
+            return result;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            ts = new CancellationTokenSource();
+            textBlock1.Text = "";
+            List<Task> tasks = new List<Task>();
+            for (int i = 2; i <= 10; i++)
+            {
+                int tmp = i;
+                Task<double> adder = Task.Factory.StartNew(() => AddMultiple(tmp), ts.Token);
+                tasks.Add(adder);
+                
+                var show = adder.ContinueWith(resultTask => textBlock1.Text += tmp.ToString() + " - " + adder.Result.ToString() + Environment.NewLine,
+                CancellationToken.None,
+                TaskContinuationOptions.OnlyOnRanToCompletion,
+                TaskScheduler.FromCurrentSynchronizationContext());
+                
+                var showCancel = adder.ContinueWith(resultTask => textBlock1.Text += tmp.ToString() + " canceled" + Environment.NewLine,
+                CancellationToken.None,
+                TaskContinuationOptions.OnlyOnCanceled, 
+                TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ts.Cancel();
+        }
+    }
+}
