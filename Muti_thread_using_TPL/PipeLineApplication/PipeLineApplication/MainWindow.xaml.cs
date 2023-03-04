2d9ba2b504b2c2f0a44110db15e3f44a94fa0df5
mesri <abde_mes@yahoo.fr>
2023-03-04 09:51:07 +0100

    Pipeline design pattern mutithreading

---
+++ Muti_thread_using_TPL/PipeLineApplication/PipeLineApplication/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PipeLineApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>    /// 
+
+    public partial class MainWindow : Window
+    {
+
+        private static String PipelineResultsFile = @"C:\Users\mesri\OneDrive\Documents\Mutlithreading\Muti_thread_using_TPL\PipeLineApplication\PipeLineApplication\data\Resulttext.txt";
+        private static String PipelineEncryptFile = @"C:\Users\mesri\OneDrive\Documents\Mutlithreading\Muti_thread_using_TPL\PipeLineApplication\PipeLineApplication\data\EncryptText.txt";
+        private static String PipelineInputFile = @"C:\Users\mesri\OneDrive\Documents\Mutlithreading\Muti_thread_using_TPL\PipeLineApplication\PipeLineApplication\data\text.txt";
+        private Stages Stage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Create the Stage object and register the event listeners to update the UI as the stages work.
+            Stage = new Stages();
+        }
+
+        private void butEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            //PipeLine Design Pattern
+            //Create queues for input and output to stages.
+            int size = 20;
+            
+            BlockingCollection<char> Buffer1 = new BlockingCollection<char>(size);
+            BlockingCollection<char> Buffer2 = new BlockingCollection<char>(size);
+            
+            TaskFactory tasks = new TaskFactory(TaskCreationOptions.LongRunning, TaskContinuationOptions.None);
+
+
+            Task Stage1 = tasks.StartNew(() => Stage.FirstStage(Buffer1, PipelineInputFile));
+            Task Stage2 = tasks.StartNew(() => Stage.StageWorker(Buffer1, Buffer2, PipelineEncryptFile));
+            Task Stage3 = tasks.StartNew(() => Stage.FinalStage(Buffer2, PipelineResultsFile));
+
+
+           Task.WaitAll(Stage2, Stage3);
+            
+            //Display the 3 files.
+            using (StreamReader inputfile = new StreamReader(PipelineInputFile))
+            {
+                while (inputfile.Peek() >= 0)
+                {
+                    tbStage1.Text = tbStage1.Text + (char)inputfile.Read();
+                }
+            }
+            
+            using (StreamReader inputfile = new StreamReader(PipelineEncryptFile))
+            {
+                while (inputfile.Peek() >= 0)
+                {
+                    tbStage2.Text = tbStage2.Text + (char)inputfile.Read();
+                }
+            }
+            
+            using (StreamReader inputfile = new StreamReader(PipelineResultsFile))
+            {
+                while (inputfile.Peek() >= 0)
+                {
+                    tbStage3.Text = tbStage3.Text + (char)inputfile.Read();
+                }
+            }
+        }
+    }
+}
---
+++ Muti_thread_using_TPL/PipeLineApplication/PipeLineApplication/Stages.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+
+namespace PipeLineApplication
+{
+    internal class Stages
+    {
+        public void FirstStage(BlockingCollection<char> output, String PipelineInputFile)
+        {
+            String DisplayData = "";
+            try
+            {
+                foreach (char C in GetData(PipelineInputFile))
+                {
+                        //Displayed characters read in from the file.              
+                    DisplayData = DisplayData + C.ToString();
+                    // Add each character to the buffer for the next stage.
+                    output.Add(C);
+                }
+
+            }
+            finally
+            {
+                output.CompleteAdding();
+                Debug.Print(string.Format("FirstStage at : {0}", DateTime.Now.ToString()));
+            }
+        }
+
+
+        public void StageWorker(BlockingCollection<char> input, BlockingCollection<char> output, String PipelineEncryptFile)
+        {
+            String DisplayData = "";
+            try
+            {
+                foreach (char C in input.GetConsumingEnumerable())
+                {
+                    //Encrypt each character.
+                    char encrypted = Encrypt(C);
+                    DisplayData = DisplayData + encrypted.ToString();
+                    //Add characters to the buffer for the next stage.
+                    output.Add(encrypted);
+                }
+                //write the encrypted string to the output file.
+                using (StreamWriter outfile = new StreamWriter(PipelineEncryptFile))
+                {
+                    outfile.Write(DisplayData);
+                }
+            }
+            finally
+            {
+                output.CompleteAdding();
+                Debug.Print(string.Format("StageWorker at : {0}", DateTime.Now.ToString()));
+            }
+        }
+
+
+        public void FinalStage(BlockingCollection<char> input, String PipelineResultsFile)
+        {
+            String OutputString = "";
+            String DisplayData = "";
+            //Read the encrypted characters from the buffer, decrypt them, and display them.
+            foreach (char C in input.GetConsumingEnumerable())
+            {
+                //Decrypt the data.
+                char decrypted = Decrypt(C);
+                //Display the decrypted data.
+                DisplayData = DisplayData + decrypted.ToString();
+                //Add to the output string.
+                OutputString += decrypted.ToString();
+            }
+            //write the decrypted string to the output file.
+            using (StreamWriter outfile = new StreamWriter(PipelineResultsFile))
+            {
+                outfile.Write(OutputString);
+            }
+
+            Debug.Print(string.Format("FinalStage at : {0}", DateTime.Now.ToString()));
+        }
+
+        public List<char> GetData(String PipelineInputFile)
+        {
+            List<char> Data = new List<char>();
+            //Get the Source data.
+            using (StreamReader inputfile = new StreamReader(PipelineInputFile))
+            {
+                while (inputfile.Peek() >= 0)
+                {
+                    Data.Add((char)inputfile.Read());
+                }
+            }
+            return Data;
+        }
+
+        public char Encrypt(char C)
+        {
+            //Take the character, convert to an int, add 1, then convert back to a character.
+            int i = (int)C;
+            i = i + 1;
+            C = Convert.ToChar(i);
+            return C;
+        }
+
+        public char Decrypt(char C)
+        {
+            int i = (int)C;
+            i = i - 1;
+            C = Convert.ToChar(i);
+            return C;
+        }
+
+
+
+
+
+
+
+    }
+
+}
