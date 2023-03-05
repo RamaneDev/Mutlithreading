ff166c1a1e7536f9723ab23f608c939bf90bccd0
mesri <abde_mes@yahoo.fr>
2023-03-05 10:57:13 +0100

    Reading asychronously using AsyncCallbackMethod of BeginRead

--- Muti_thread_using_TPL/FileReadAsync/FileReadAsync/Program.cs
+++ Muti_thread_using_TPL/FileReadAsync/FileReadAsync/Program.cs
@@ -1,39 +1,57 @@
 ﻿using System;
 using System.IO;
+using System.Text;
+using System.Threading;
 
 namespace FileReadAsync
 {
     internal class Program
     {
+        static byte[] buffer2;
         static void Main(string[] args)
         {
-            byte[] FileData = new byte[1000];
+            
+            buffer2 = new byte[5000];
+
             string path = "C:\\Users\\mesri\\OneDrive\\Documents\\Mutlithreading\\Muti_thread_using_TPL\\FileReadAsync\\FileReadAsync\\data\\text.txt";
-            FileStream FS = new FileStream(path, FileMode.Open, 
-                                                 FileAccess.Read, 
-                                                 FileShare.Read,
-                                                 1024,
-                                                 FileOptions.Asynchronous);
-            
+            // Open a file for reading
+            FileStream FS = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            // Begin reading data asynchronously from the file
+           // byte[] buffer = new byte[1024];
             Console.WriteLine("To start async read press return.");
+            
             Console.ReadLine();
-            
-            IAsyncResult result = FS.BeginRead(FileData, 0, FileData.Length, null, null);
-            
+            IAsyncResult result = FS.BeginRead(buffer2, 0, buffer2.Length, AsyncCallbackMethod, FS);
             // Work being done while we wait on the async //read.
-            Console.WriteLine("\r\n");
+           
             Console.WriteLine("Doing Some other work here. \r\n");
-            Console.WriteLine("\r\n");
-            
-            //Calling EndRead will block the main thread //until the async work has finished.
-            int num = FS.EndRead(result);
+       
+            Console.ReadLine();
+        }
+
+        static void AsyncCallbackMethod(IAsyncResult ar)
+        {
+            byte[] buffer = new byte[5000];
+            // Get the file stream object and end the asynchronous read operation
+            FileStream FS = (FileStream)ar.AsyncState;
+            int bytesRead = FS.EndRead(ar);
+
+            // Make sure to close the FileStream.
             FS.Close();
 
-            Console.WriteLine("Read {0} bytes from the file. \r\n", num);
-            Console.WriteLine("Is the async read completed - {0}. \r\n", result.IsCompleted.ToString());
-            Console.WriteLine(BitConverter.ToString(FileData));
-            Console.ReadLine();
+            //Now, write out the results.
+            Console.WriteLine("Read {0} bytes from the file. \r\n", bytesRead);
+           
+            Console.WriteLine("Is the async read completed - {0}. \r\n", ar.IsCompleted.ToString());
+           
+            Console.WriteLine("numbre of bytes read from stream : ", bytesRead);
 
+            Console.WriteLine(BitConverter.ToString(buffer2));
+
+            // Begin reading data asynchronously from the file again
+            //buffer = new byte[1024];
+            //fileStream.BeginRead(buffer, 0, buffer.Length, AsyncCallbackMethod, fileStream);   // we call recall if needed to read more bytes
         }
     }
 }
